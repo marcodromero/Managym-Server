9a6282c7425fb7a74b9e71a69ae9462939d8614f
Marco Romero <marcod.romero@gmail.com>
2023-07-13 15:20:03 -0300

    hide morgan

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan("dev"));
+//app.use(morgan("dev"));
 app.use(cors());
 
 // Api routes
